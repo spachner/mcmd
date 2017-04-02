8cd3b677dd0d0a695391401b7cad9bd5792d8444
Stefan Pachner <stefan.pachner@kinexon.com>
2017-04-02 16:49:28 +0200

    Added con file usage

--- mcmd.rb
+++ mcmd.rb
@@ -1,18 +1,64 @@
 require 'open3'
+require 'yaml'
 
-$cfgUseLog     = false # will hang app sometimes
+# Usage:
+#   cshoes mcmd.rb [<my-conf-file>.yaml]
+#
+
+$cfgUseLog     = false # Experimental: Log widget will hang app sometimes
 $cfgShowModify = false
-
-$cfgHomeDir="/Users/spachner/Documents/dev/spr/mcmd"
-# Prepend commands relativ to homeDir with './'
-$cfgSpec = [
+$cfgHomeDir    = "/Users/spachner/Documents/dev/spr/mcmd"
+$specFileName  = 'mcmd-conf.yaml' # defaile file name, my be overritten by command line argument
+$exampleSpec   =
+[
     ['ls -1',                'ls -1'],
     ['ls -l',                'ls -l'],
     ['stdout err test',      './testcmd.bash'],
     ['pwd',                  'pwd'],
     ['cat large',            'cat /var/log/monthly.out'],
-    ['edit mcmd.rb',         'atom /Users/spachner/Documents/dev/spr/mcmd/mcmd.rb'],
+    ['edit mcmd.rb',         'atom /Users/spachner/Documents/dev/spr/mcmd/mcmd.rb']
 ]
+
+trap("SIGINT") {
+    puts "Exit pending. Please click into GUI window to finalize exit"
+    Shoes.quit
+}
+
+def dumpSpec s
+    s.size.times do |c|
+        puts "CmdText >#{s[c][0]}<,\tcmd >#{s[c][1]}<"
+    end
+end
+
+def readSpec fileName
+    if !File.file? fileName
+        abort "Cannot read file >#{fileName}<"
+    end
+    $cfgSpec = YAML.load_file fileName
+    puts "Read spec from >#{fileName}<"
+end
+
+def writeSpec fileName, spec
+    File.open(fileName, 'w') { |f| f.write spec.to_yaml }
+    puts "Wrote spec to >#{fileName}<"
+end
+
+if ARGV.size-1 < 1
+    if !File.file? $specFileName
+        puts "No arg given, creating default specFile >#{$specFileName}<"
+        writeSpec $specFileName, $exampleSpec
+        readSpec  $specFileName
+    else
+        puts "No arg given, reading default specFile >#{$specFileName}<"
+        readSpec  $specFileName
+    end
+else
+    $specFileName = ARGV[1]
+    puts "Arg given, reading spec from file >#{$specFileName}<"
+    readSpec $specFileName
+end
+
+#dumpSpec $cfgSpec
 
 Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
     @spec    = $cfgSpec
@@ -23,44 +69,36 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
         @log.text = '' if @useLog
     end
 
-    def appendLog(str)
+    def appendLog str
         puts str
         @log.append str    if @useLog
         @log.scroll_to_end if @useLog
     end
 
-    def dumpSpec
-        @spec.size.times do |c|
-            puts "CmdText >#{@spec[c][0]}<,\tcmd >#{@spec[c][1]}<"
-        end
-    end
-
-    #dumpSpec
-
     lrStackMarginTop = 10
-    tableHeight = 30
-    lstackWidth = 150
-    rstackWidth = 350
-    paraSize    = 9
-    checkMarginTop  = 4
-    checkMarginLeft = 10
-    paraMarginTop   = tableHeight/2 - 7
-    paraMarginLeft  = 0
-    paraMarginRight = 10
-    logMarginLeft   = 6
+    tableHeight      = 30
+    lstackWidth      = 150
+    rstackWidth      = 350
+    paraSize         = 9
+    checkMarginTop   = 4
+    checkMarginLeft  = 10
+    paraMarginTop    = tableHeight/2 - 7
+    paraMarginLeft   = 0
+    paraMarginRight  = 10
+    logMarginLeft    = 6
 
     @mainStack = stack do
         flow do
             @lstack = stack :margin_top => lrStackMarginTop, :width => lstackWidth do
                 #---------------------------------------------------------------
                 stack :height => tableHeight do
-                    @sethd = button("Set home dir", :width => lstackWidth)
+                    @sethd = button "Set home dir", :width => lstackWidth
                 end
                 #---------------------------------------------------------------
                 @button = Array.new
                 @spec.size.times do |c|
                     stack :height => tableHeight do
-                        @button[c] = button(@spec[c][0], :width => lstackWidth)
+                        @button[c] = button @spec[c][0], :width => lstackWidth
                     end
                 end
             end
@@ -84,7 +122,7 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
                     stack :height => tableHeight do
                         @cmd[c] = edit_line(:width => rstackWidth) do | edit |
                             @spec[c][1] = edit.text
-                            dumpSpec
+                            dumpSpec @spec
                         end
                         @cmd[c].text = @spec[c][1]
                     end
@@ -95,14 +133,13 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
 
         #-----------------------------------------------------------------------
         flow do
-            button("Quit") { Shoes.quit }
-            button("Clear Log") { clearLog }
-
-            button("Test") do
-                puts '-------set @mainStack height'
-                #Window.style(:width => 100)#:height => 100,
-                self.resize(100,100)
-            end
+            button("Quit")      { Shoes.quit }
+            button("Clear Log") { clearLog   }
+            #button "Test" do
+            #    puts '-------set @mainStack height'
+            #    #Window.style(:width => 100)#:height => 100,
+            #    self.resize(100,100)
+            #end
         end
 
         flow do
@@ -132,7 +169,9 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
 
     def logOnOffCheckState
         if @logOnOff.checked?
-            @useLog = true
+            @useLog = confirm("Sorry, Log widget is expermental. Expect freezing app on large output?")
+            puts "----------Answer #{@useLog }"
+            @logOnOff.checked = @useLog
             @log.show
         else
             @useLog = false
@@ -147,20 +186,21 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
 
             clearLog
             exe = @spec[c][1].split[0]   # make from e.g. "ls -1" -> "ls"
-            if (exe.start_with?'./')
+            if exe.start_with?'./'
                 exe = @homeDir + '/' + exe
             end
             ok = system "which #{exe}"
-            if (!ok)
+            if !ok
                 appendLog ">#{@spec[c][1]}< -> >#{exe}< not executable"
             else
-                if (@cmdActive)
+                if @cmdActive
                     appendLog "Other cmd active, ignored"
                     return
                 end
                 @cmdActive = true
                 t = Thread.new do
-                    Open3.popen2e(@spec[c][1], :chdir => @homeDir) do |stdin, stdout_and_stderr, wait_thr|
+                    Open3.popen2e(@spec[c][1], :chdir => @homeDir) do
+                        | stdin, stdout_and_stderr, wait_thr |
                         #stdin.close
                         prependStr = "pid #{wait_thr[:pid]}: "
                         stdout_and_stderr.each do |line|
@@ -177,13 +217,8 @@ Shoes.app(title: "mcmd",  resizable: true) do #width: 1000, height: 500,
 
     # init ---------------------------------------------------------------------
     initThread = Thread.new do
-        sleep(0.5)
+        sleep 0.5
         modifyCheckState
         logOnOffCheckState
     end
 end
-
-trap("SIGINT") {
-    puts "Click into window to quit"
-    Shoes.quit
-}
