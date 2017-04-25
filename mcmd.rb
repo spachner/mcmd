ca2fdbdb3a1cd3a34d1caabdb948fd9302f98f4a
Stefan Pachner <stefan.pachner@kinexon.com>
2017-04-25 19:16:34 +0200

    Buttons are in a flow now for easy resize ability

--- mcmd.rb
+++ mcmd.rb
@@ -55,8 +55,11 @@ def checkAndSetNewBaseDir dir
     end
 end
 
+$mainWidth  = 800
+$mainHeight = 500
+
 #--- Shoes main ----------------------------------------------------------------
-Shoes.app(title: "mcmd", resizable: true, width: 300, height: 1000) do
+Shoes.app(title: "mcmd", resizable: true, width: $mainWidth, height: $mainHeight) do
     @useLog  = $cfgUseLog   # init value, changed by checkbox click
 
     def clearLog
@@ -70,71 +73,101 @@ Shoes.app(title: "mcmd", resizable: true, width: 300, height: 1000) do
         @mainStack.refresh_slot if @logOnOff.checked? # force redraw slot, slows down execution but command output is seen immediately in log
     end
 
-    lrStackMarginTop = 10
-    tableHeight      = 30
-    lstackWidth      = 250
-    rstackWidth      = 350
-    paraSize         = 9
-    checkMarginTop   = 4
-    checkMarginLeft  = 10
-    paraMarginTop    = tableHeight/2 - 7
-    paraMarginLeft   = 0
-    paraMarginRight  = 10
-    logMarginLeft    = 6
-    logHeight        = 300
-    logWidth         = 600
+    @lrStackMarginTop = 10
+    @tableHeight      = 30
+    @lstackWidth      = 250
+    @rstackWidth      = 350
+    @paraSize         = 9
+    @checkMarginTop   = 4
+    @checkMarginLeft  = 10
+    @paraMarginTop    = @tableHeight/2 - 7
+    @paraMarginLeft   = 0
+    @paraMarginRight  = 10
+    @logMarginLeft    = 6
+    @logHeight        = 300
+    @logWidth         = $mainWidth
 
-    @mainStack = stack do
-        flow do
-            @lstack = stack :margin_top => lrStackMarginTop, :width => lstackWidth do
-                #---------------------------------------------------------------
-                stack :height => tableHeight do
-                    @sethd = button "Set home dir", :width => lstackWidth do
-                        checkAndSetNewBaseDir @hd.text
-                    end
+    def registerEventHandlers
+        # Event handler ------------------------------------------------------------
+        $spec.getCmds.size.times do |cmdIdx|
+            @button[cmdIdx].click do
+                clearLog
+
+                cmdWithVars = $spec.getCmdTxtByIdx(cmdIdx)
+                cmd = cmdWithVars
+                puts "before var sub >#{cmd}<" if $debug
+                cmd = $exe.substitute cmd, $spec.getCmds
+                puts "after  var sub >#{cmd}<" if $debug
+
+                if cmdWithVars != cmd
+                    appendLog "try to execute >#{cmdWithVars}< results in >#{cmd}<\n"
+                else
+                    appendLog "try to execute >#{cmd}<\n"
                 end
-                #---------------------------------------------------------------
-                @button = Array.new
-                $spec.getCmds.size.times do |cmdIdx|
-                    stack :height => tableHeight do
 
-                        buttonText = $exe.substitute $spec.getBtnTxtByIdx(cmdIdx), $spec.getCmds
-
-                        @button[cmdIdx] = button buttonText, :width => lstackWidth
-                    end
+                executable = $exe.getCmdExecutable cmd
+                if $exe.isCmdExecutable executable
+                    $exe.exeCmd cmd
+                else
+                    appendLog ">#{cmdWithVars}< results in >#{cmd}< is not executable"
+                    signalCmdEndError
                 end
             end
+        end
+    end
 
-            @rstack = stack :margin_top => lrStackMarginTop, :width => rstackWidth do
-                #---------------------------------------------------------------
-                stack :height => tableHeight do
-                    @hd = edit_line :width => rstackWidth # new base dir is stored by return or button click
-                end
+    def createFlowCmd withEditLines
+        f = flow do
+            #@lstack = stack :margin_top => lrStackMarginTop, :width => lstackWidth do
+            #---------------------------------------------------------------
+            #stack :height => tableHeight do
+            @sethd = button "Set home dir", :width => @lstackWidth do
+                checkAndSetNewBaseDir @hd.text
+            end
+            #end
+            #---------------------------------------------------------------
+            #stack :height => tableHeight do
+            if withEditLines
+                @hd = edit_line :width => @rstackWidth # new base dir is stored by return or button click
+                #end
                 @hd.text = $spec.getBaseDir
                 @hd.finish = proc { |slf|
                     puts "New base dir >#{slf.text}<\n" if $debug
                     checkAndSetNewBaseDir slf.text
                 }
-                #---------------------------------------------------------------
-                @cmd = Array.new
-                $spec.getCmds.size.times do |cmdIdx|
-                    stack :height => tableHeight do
-                        @cmd[cmdIdx] = edit_line(:width => rstackWidth) do | edit |
-                            $spec.setCmdTxt cmdIdx, edit.text, false
-                        end
-                        @cmd[cmdIdx].text = $spec.getCmdTxtByIdx cmdIdx
+            end
+
+            #---------------------------------------------------------------
+            @button = Array.new
+            @cmd    = Array.new
+            $spec.getCmds.size.times do |cmdIdx|
+                #stack :height => tableHeight do
+
+                buttonText = $exe.substitute $spec.getBtnTxtByIdx(cmdIdx), $spec.getCmds
+
+                @button[cmdIdx] = button buttonText, :width => @lstackWidth
+                #end
+
+                if withEditLines
+                    @cmd[cmdIdx] = edit_line(:width => @rstackWidth) do | edit |
+                        $spec.setCmdTxt cmdIdx, edit.text, false
                     end
+                    @cmd[cmdIdx].text = $spec.getCmdTxtByIdx cmdIdx
+                    #end
                     @cmd[cmdIdx].finish = proc { |slf|
                         puts "New cmd >#{slf.text}<\n" if $debug
                         $spec.setCmdTxt cmdIdx, slf.text, true
                     }
                 end
-                #---------------------------------------------------------------
             end
         end
+        registerEventHandlers
+        f
+    end
 
+    def createFlowCtrl
         #-----------------------------------------------------------------------
-        flow do
+        @flowControl = flow do
             button("Quit")          { Shoes.quit }
             button("Kill Command")  { $exe.kill }
             button("Clear Log")     { clearLog   }
@@ -146,27 +179,38 @@ Shoes.app(title: "mcmd", resizable: true, width: 300, height: 1000) do
         end
 
         flow do
-            @modify = check checked: $cfgShowModify, :margin_left => checkMarginLeft, :margin_top => checkMarginTop do
+            @modify = check checked: $cfgShowModify, :margin_left => @checkMarginLeft, :margin_top => @checkMarginTop do
                 modifyCheckState
             end
-            para "Modify", size: paraSize, :margin_top => paraMarginTop, :margin_left => paraMarginLeft, :margin_right => paraMarginRight
+            para "Modify", size: @paraSize, :margin_top => @paraMarginTop, :margin_left => @paraMarginLeft, :margin_right => @paraMarginRight
 
-            @logOnOff = check checked: $cfgUseLog, :margin_left => checkMarginLeft, :margin_top => checkMarginTop do
+            @logOnOff = check checked: $cfgUseLog, :margin_left => @checkMarginLeft, :margin_top => @checkMarginTop do
                 logOnOffCheckState
             end
-            para "Log", size: paraSize, :margin_top => paraMarginTop, :margin_left => paraMarginLeft, :margin_right => paraMarginRight
+            para "Log", size: @paraSize, :margin_top => @paraMarginTop, :margin_left => @paraMarginLeft, :margin_right => @paraMarginRight
         end
 
         #-----------------------------------------------------------------------
-        @log = edit_box :margin_left => logMarginLeft, :width => logWidth, :height => logHeight, :resizable => true
+        @log = edit_box :resizable => true, :margin_left => @logMarginLeft, :width => @logWidth, :height => @logHeight
         #-----------------------------------------------------------------------
+    end
+
+    @mainStack = stack do
+        @flowCmd  = createFlowCmd true
+        createFlowCtrl
     end # stack
 
     def modifyCheckState
         if @modify.checked?
-            @rstack.show
+            #@rstack.show
+            @flowCmd.clear {
+                createFlowCmd true
+            }
         else
-            @rstack.hide
+            #@rstack.hide
+            @flowCmd.clear {
+                createFlowCmd false
+            }
         end
     end
 
@@ -181,43 +225,16 @@ Shoes.app(title: "mcmd", resizable: true, width: 300, height: 1000) do
         end
     end
 
-    # Event handler ------------------------------------------------------------
-    $spec.getCmds.size.times do |cmdIdx|
-        @button[cmdIdx].click do
-            clearLog
-
-            cmdWithVars = $spec.getCmdTxtByIdx(cmdIdx)
-            cmd = cmdWithVars
-            puts "before var sub >#{cmd}<" if $debug
-            cmd = $exe.substitute cmd, $spec.getCmds
-            puts "after  var sub >#{cmd}<" if $debug
-
-            if cmdWithVars != cmd
-                appendLog "try to execute >#{cmdWithVars}< results in >#{cmd}<\n"
-            else
-                appendLog "try to execute >#{cmd}<\n"
-            end
-
-            executable = $exe.getCmdExecutable cmd
-            if $exe.isCmdExecutable executable
-                $exe.exeCmd cmd
-            else
-                appendLog ">#{cmdWithVars}< results in >#{cmd}< is not executable"
-                signalCmdEndError
-            end
-        end
-    end
-
     def signalCmdRun
-        @lstack.background yellow
+        @flowCmd.background yellow
     end
 
     def signalCmdEndSuccessful
-        @lstack.background green
+        @flowCmd.background green
     end
 
     def signalCmdEndError
-        @lstack.background red
+        @flowCmd.background red
     end
 
     $exe.setLogCB lambda {|str| appendLog str}
